1849c76809d35f09d13ec582eb899eb221f4f18a
ustjavafsdb202 <carmelsandra22@gmail.com>
2022-11-11 10:00:18 +0530

    changes updated

--- src/main/java/com/student/ust/entity/Student.java
+++ src/main/java/com/student/ust/entity/Student.java
@@ -8,18 +8,20 @@ import java.util.Set;
 
 @Entity
 @Data
+@Table(name="student_ust_identitymappedby")
 public class Student {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int Student_id;
     private int rollNo;
     private  String name;
     private int age;;
     private LocalDateTime createDate;
     private LocalDateTime modifyDate;
-@OneToMany(cascade=CascadeType.ALL)
+@OneToMany(cascade=CascadeType.ALL,fetch=FetchType.LAZY,mappedBy="student")
 @Column(name="book_id")
     private Set<Book> bookSet;
+
 
 
 
